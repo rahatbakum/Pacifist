ee77fddc2b642255d220f8674273629b4b4b0118
rahatbakum <vovnedj@gmail.com>
2022-01-09 10:44:53 +0300

    Android, Prefabs, SceneManagement
    Moved to Android
    Automatic Scene Management in portals
    Prefabs

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -10,6 +10,7 @@ public class PortalController : MonoBehaviour
     [Space]
     [SerializeField] private string playerTag = "Player";
     [SerializeField] private string nextSceneName = "Level01";
+    [SerializeField] private bool isAutomatic = true; //is Automatically set scene name
 
     private SpriteRenderer thisSpriteRenderer;
 
@@ -42,8 +43,35 @@ public class PortalController : MonoBehaviour
         }
     }
 
+    string intToString (int number, int stringLength)
+    {
+        string stringNumber = number.ToString();
+        string ans = "";
+        for(int i = 0; i < stringLength; i++)
+        {
+            if (stringNumber.Length > i)
+                ans = stringNumber[stringNumber.Length - i - 1] + ans;
+            else
+                ans = "0" + ans;
+        }
+        return ans;
+    }
+
+    string nextNameByName(string name)
+    {
+        int sceneNumber;
+        if(!int.TryParse(name.Substring(name.Length-2, 2), out sceneNumber))
+        {
+            return nextSceneName;
+        }
+        return name.Substring(0,name.Length-2) + intToString(sceneNumber + 1, 2);
+    }
+
     void NextLevel()
     {
-        SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        string nextSceneNameFinal = nextSceneName;
+        if (isAutomatic)
+            nextSceneNameFinal = nextNameByName(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(nextSceneNameFinal, LoadSceneMode.Single);
     }
 }
--- Assets/Scripts/SmoothCameraFollower.cs
+++ Assets/Scripts/SmoothCameraFollower.cs
@@ -9,8 +9,10 @@ public class SmoothCameraFollower : MonoBehaviour
     [SerializeField] private Vector3 offset = new Vector3(0, 0, 0);
     [SerializeField] private bool isSaveCurrentOffsetOnStart = false;
     [SerializeField] private float speed = 10f;
+    //[SerializeField] private float outrunCoefficient = 2f;
 
     private float fixedZPosition;
+    private Rigidbody2D targetRigidBody;
 
     void Start()
     {
@@ -20,13 +22,33 @@ public class SmoothCameraFollower : MonoBehaviour
         }
 
         fixedZPosition = transform.position.z;
+
+        targetRigidBody = targetObject.GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
+        moveCamera();
+    }
+
+    Vector3 calculateDesirePosition()
+    {
+        Vector3 ans = targetObject.position + offset;
+        // if (targetRigidBody)
+        // {
+        //     Vector3 velocityOffset = new Vector3(targetRigidBody.velocity.x, targetRigidBody.velocity.y, 0f);
+        //     ans = ans + velocityOffset * outrunCoefficient * Time.fixedDeltaTime;
+        // }
+        ans = new Vector3(ans.x, ans.y, fixedZPosition);
+
+        return ans;
+    }
+
+    void moveCamera()
+    {
         if (Mathf.Abs(Vector2.Distance(transform.position, targetObject.position)) > 0.05f)
         {
-            transform.position = Vector2.Lerp(transform.position, targetObject.position + offset, speed * Time.deltaTime);
+            transform.position = Vector2.Lerp(transform.position, calculateDesirePosition(), speed * Time.fixedDeltaTime);
             transform.position = new Vector3(transform.position.x, transform.position.y, fixedZPosition);
         }
     }
